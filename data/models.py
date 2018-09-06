38ce663437a5d8a2b31336b42a45aa08b5bc6c56
JohnVonNeumann <louiswillcock@gmail.com>
2018-09-06 15:39:38 +1000

    UPDATE models.py - create basic data model

--- data/models.py
+++ data/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Data(models.Model):
+    first_field = models.CharField(max_length=200)
+    second_field = models.CharField(max_length=200)
+    third_field = models.CharField(max_length=200)
+    fourth_field = models.CharField(max_length=200)
