9ff7a91c457cc7e54c5bb13c0602645fa4099282
JohnVonNeumann <louiswillcock@gmail.com>
2018-09-06 15:37:00 +1000

    UPDATE data app - create pages for data insert and select

--- data/urls.py
+++ data/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('insert_data', views.insert_data, name='insert_data'),
+    path('select_data', views.select_data, name='select_data'),
 ]
--- data/views.py
+++ data/views.py
@@ -3,3 +3,9 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the data index.")
+
+def insert_data(request):
+    return HttpResponse("You are at the data insertion page.")
+
+def select_data(request):
+    return HttpResponse("You are at the data selection page.")
